4cd27f337fb0e7627125cb38cbaa0c160fbd1e1d
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-04-15 12:47:51 -0400

    Update pwc.py

--- pwc.py
+++ pwc.py
@@ -3,7 +3,7 @@ import pyperclip
 from random import randint
 
 IGNORED_CHARS = [34, 42, 44, 46, 58, 59, 60, 62, 94, 96, 123, 124, 125] + [ord(item) for item in sys.argv[2:]]
-pw_len = sys.argv[1] if len(sys.argv) > 1 else 35
+pw_len = sys.argv[1] if len(sys.argv) > 0 else 35
 pw = ""
 i = 0
 while i < pw_len:
