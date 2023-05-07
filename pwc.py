090a5eed655476b9c6c56f4b6217b1a6618737a7
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-05-07 12:41:47 -0400

    Update pwc.py

--- pwc.py
+++ pwc.py
@@ -3,10 +3,10 @@ import pyperclip
 from random import randint
 
 IGNORED_CHARS = [34, 42, 44, 46, 58, 59, 60, 62, 94, 96, 123, 124, 125] + [ord(item) for item in sys.argv[2:]]
-pw_len = sys.argv[1] if len(sys.argv) > 0 else 35
+pw_len = sys.argv[1] if len(sys.argv) > 1 else 35
 pw = ""
 i = 0
-while i < pw_len:
+while i < int(pw_len):
     charcode = randint(33, 126)
     if charcode not in IGNORED_CHARS:
         pw += chr(charcode)
